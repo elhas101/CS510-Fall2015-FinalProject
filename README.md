9531acf8766d0550b5be382815dded9d86b7f35f
Abdal Elhassani <elhas101@mail.chapman.edu>
2015-12-18 06:34:06 +0000

    final update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # CS510-Fall2015-FinalProject
 
-Final project submission
+Final project submission for CS510 FALL 2015.
+
+To run the program, set settings under generate_output.sh then execute the program: 
+>sh ./generate_output.sh
+
+To generate 2d images of juliaset and mandelbrot: 
+>python ./plot_data.py mandelbrot.csv && python ./plot_data.py juliaset.csv
--- generate_output.sh
+++ generate_output.sh
@@ -1,10 +1,10 @@
 
-echo "Executing julia and mandelbrot..."
+echo "Executing juliaset and mandelbrot..."
 # Execute make for julia and mandelbrot
 (cd C-JuliaSets && make julia)
 (cd Cpp-Mandelbrot && make mandelbrot)
 
 echo "Generating datafiles..."
 # Generate default csv datafiles in main directory
-C-JuliaSets/julia -0.1 0.1 -0.1 0.1 1920 1920 -1.037 0.17 > juliaset.csv
-Cpp-Mandelbrot/mandelbrot -0.1 0.1 -0.1 0.1 1920 1920 > mandelbrot.csv 
+C-JuliaSets/julia -2 2 -2 2 1920 1920 -1.037 0.17 > juliaset.csv
+Cpp-Mandelbrot/mandelbrot -2 2 -2 2 1920 1920 > mandelbrot.csv 
